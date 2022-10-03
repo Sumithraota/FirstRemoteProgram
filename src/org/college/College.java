abe48de98ea14fbeb6fa3b694638bcc718eed7ea
Sumithraota <OTASumithra27@gmail.com>
2022-10-03 17:40:36 +0530

    first commit

---
+++ src/org/college/College.java
@@ -0,0 +1,33 @@
+package org.college;
+
+public class College {
+	private void collegeName() {
+		// TODO Auto-generated method stub
+     System.out.println("ACET");
+	}
+	public static void main(String[] args) {
+		College c = new College();
+        c.collegeName();
+        
+        Student s = new Student();
+        s.studentName();
+        
+        Hostel h =new Hostel();
+        h.hostelName();
+       
+
+        
+        Dept d = new Dept(); 
+        d.deptName();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+	}
+
+}
---
+++ src/org/college/Student.java
@@ -0,0 +1,8 @@
+package org.college;
+
+public class Student {
+	public void studentName() {
+		// TODO Auto-generated method stub
+		System.out.println("Sumithra");
+	}
+}
